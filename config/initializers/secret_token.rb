4d0295b247c6d5e085122472f4b5c626366e60d9
FionaDL <fionadlapham@gmail.com>
2020-03-23 23:46:43 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails40Sample::Application.config.secret_key_base = '7ca10f142398024c35165d70ef2d9f63ade20825f88d9ed1797cb97a83e392079d79c015f0fd43ef174e49ae0618fbf01010e1bafe47961059cc969ea54b75e5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails40Sample::Application.config.session_store :cookie_store, key: '_rails_4_0_sample_session'
